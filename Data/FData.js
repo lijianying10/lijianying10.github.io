7dfbbe1a855d38ea52d9660db47e567bf96bcf03
lijianying10 <lijianying12@gmail.com>
2014-05-10 12:22:29 +0800

    inital commit

---
+++ Data/FData.js
@@ -0,0 +1,26 @@
+/**
+ * Created by lijianying on 14-5-9.
+ */
+//This Json File define the File data in this system
+
+    //ID just help input
+var FData=[
+    {ID:1,title:'test',time:'2014年5月10日00:11:24',FileName:'main.pdf'},
+    {ID:1,title:'test',time:'2014年5月10日00:11:24',FileName:'main.pdf'},
+    {ID:1,title:'test',time:'2014年5月10日00:11:24',FileName:'main.pdf'},
+    {ID:1,title:'test',time:'2014年5月10日00:11:24',FileName:'main.pdf'},
+    {ID:1,title:'test',time:'2014年5月10日00:11:24',FileName:'main.pdf'}
+]
+
+var Nodes=[
+    {Name:"test32143",Child:[
+        {Name:"test1",FileName:"main.pdf"},{Name:"test1",FileName:"default.pdf"}
+    ],FileName:""},
+    {Name:"test3253456436",Child:[],FileName:"default.pdf"},
+    {Name:"test65756",Child:[],FileName:"main.pdf"},
+    {Name:"test23235",Child:[{Name:"",FileName:""}],FileName:""}
+]
+
+B_2013Apr2 = {fn:"2013apr2.pdf",title:"Project build by CMake"}
+B_2013May1 = {fn:"2013may1.pdf",title:"Fortran Project build by CMake"}
+B_2013May2 = {fn:"2013may2.pdf",title:"Install Linux System using PXE"}
