20354dfac18311cb5d691b90f83a2686e155659f
lijianying10 <lijianying12@gmail.com>
2014-05-10 14:56:43 +0800

    add about page and fix native url

--- js/control.js
+++ js/control.js
@@ -5,8 +5,7 @@ window.onresize=function(){autoadjestreader();};
 autoadjestreader();
 loadlabel();
 
-function byid(id)
-{return document.getElementById(id);}
+
 
 function setheigh(){
     byid('pdffile').style.height=byid('inputheigh').value+'px';
@@ -32,29 +31,6 @@ function readerAdjest()
     var    s  = "\r\n网页正文部分上："+  window.screenTop;
     console.log(s);
 }
-
-function SetCookie(name,value)//两个参数，一个是cookie的名子，一个是值
-{
-    var Days = 30; //此 cookie 将被保存 30 天
-    var exp = new Date();    //new Date("December 31, 9998");
-    exp.setTime(exp.getTime() + Days*24*60*60*1000);
-    document.cookie = name + "="+ escape (value) + ";expires=" + exp.toGMTString();
-}
-
-function getCookie(name)//取cookies函数
-{
-    var arr = document.cookie.match(new RegExp("(^| )"+name+"=([^;]*)(;|$)"));
-    if(arr != null) return unescape(arr[2]); return null;
-}
-
-function delCookie(name)//删除cookie
-{
-    var exp = new Date();
-    exp.setTime(exp.getTime() - 1);
-    var cval=getCookie(name);
-    if(cval!=null) document.cookie= name + "="+cval+";expires="+exp.toGMTString();
-}
-
 
 function changeBook(jjj)
 {
